7b2b27f75cf84d9904afedf21454b79a5dbd4d15
NazarFedus <simpleprograming7@gmail.com>
2022-12-30 20:07:02 +0000

    homework has done

---
+++ index.js
@@ -0,0 +1,11 @@
+// перша задача:
+alert(`Hello, my name is ${prompt('What is your name?')}. I am ${prompt('How old are you?')} years old. I want to be a ${prompt('What do you want to be?')}.`);
+
+
+
+// друга задача:
+const num = 12345;
+
+let result = String(num).split('').join(' ');
+
+console.log(result);
