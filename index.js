4e246b3fe7196ef41c44f2957db25c6d0e0d58c6
NazarFedus <simpleprograming7@gmail.com>
2023-03-11 11:15:26 +0000

    add second block (productsList)

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ container.appendChild(listProducts)
 container.appendChild(blockInfo)
 
 
-
 // Get data
 async function fetchData() {
      try {
@@ -33,38 +32,43 @@ async function processProducts() {
      const products = await fetchData();
      console.log(products);
 
-     // types (категорії)
-     const types = [];
-     products.forEach(e => !types.includes(e.category) ? types.push(e.category) : null)
-     types.forEach(type => {
-          console.log(type)
-          const tab = document.createElement('button');
-          tab.classList.add('tab')
-          tab.innerText = type
-          tabs.appendChild(tab)
-
+     // categories
+     const categories = [];
+     products.forEach(e => !categories.includes(e.category) ? categories.push(e.category) : null)
+     categories.forEach(type => {
+          createTab(tabs, type);
      })
 
-
-     console.log(types)
+     const tabList = tabs.querySelectorAll('.tab')
+          tabList.forEach(element => {
+               element.addEventListener('click', () => {
+                    tabList.forEach(e => e.classList.remove('active'))
+                    element.classList.add('active')
+                    showListOfProducts(element.innerText, products)
+               })
+          })
 }
 
 processProducts();
 
 
+function createTab(venue, value){
+     const tab = document.createElement('button');
+          tab.classList.add('tab')
+          tab.innerText = value
+          venue.appendChild(tab)
+}
 
+function showListOfProducts(category, arrayOfProducts){
+     // filter our category:
+     const products = arrayOfProducts.filter(item => item.category === category);
 
+     // clear boxInfo from previous list of products:
+     const productsList = listProducts.querySelectorAll('.tab')
+     if(productsList) productsList.forEach(e => listProducts.removeChild(e))
 
-
-
-
-
-
-// addClass(tabs, 'block')
-// // addClass(listProducts)
-// // addClass()
-
-
-
-// // add class;
-// const addClass = (element, class) => element.classList.add(class);
+     // create boxInfo "products list":
+     products.forEach(item => {
+          createTab(listProducts, item.title)
+     })
+}
