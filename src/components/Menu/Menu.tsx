9b23f8af0af735d76c45ed7fb4ec1a18b6ba51f3
NazarFedus <simpleprograming7@gmail.com>
2023-04-30 22:30:52 +0100

    create function ShowOrder

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -7,9 +7,18 @@ import { IContextProps } from "../../reducers/contextTypes";
 const Menu: FC = () => {
   const { state } = useContext(MenuContext);
   console.log(state);
+
+  const showOrder = (): string => {
+     const result = [];
+     for(let key in state){
+          let chosen = state[key].find((e: IProduct) => e.choosed === true)
+          if(chosen !== undefined) result.push(chosen.name);
+     }
+     return result.filter(e => e !== undefined).join('+')
+  }
   return (
     <div className="flex flex-col w-full">
-      <div className="flex-[10] bg-white">dskfkds</div>
+      <div className="flex-[10] bg-white">{showOrder()}</div>
       <div className="flex flex-row flex-[90]">
         <div className="flex-[33] bg-red-600">
           {state.burgers.map((burger: IProduct) => (
