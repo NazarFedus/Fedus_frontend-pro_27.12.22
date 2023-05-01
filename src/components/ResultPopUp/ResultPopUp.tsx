1de7776a87cec1351980d25dc3db6ab156a3ec91
NazarFedus <simpleprograming7@gmail.com>
2023-05-01 08:46:07 +0100

    refactor code

--- src/components/ResultPopUp/ResultPopUp.tsx
+++ src/components/ResultPopUp/ResultPopUp.tsx
@@ -1,24 +1,16 @@
 import { FC, useState } from "react";
+import { IResultPopUp } from "./ResultPopUpTypes";
 
-export default function ResultPopUp({show, handleClose}: {show: boolean, handleClose: () => void}){
-  const [isOpen, setIsOpen] = useState<boolean>(show);
-
-  function togglePopup() {
-    setIsOpen(!isOpen);
-  }
-
+export default function ResultPopUp({show, handleClose}: IResultPopUp){
   return (
     <div className="relative">
-      {isOpen && (
+      {show && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg shadow-md">
             <h2 className="text-lg font-bold mb-4">Popup Content</h2>
             <p>Here's some content for the popup!</p>
             <button
-              onClick={() => {
-                togglePopup(),
-                handleClose()
-              }}
+              onClick={handleClose}
               className="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded"
             >
               Close Popup
---
+++ src/components/ResultPopUp/ResultPopUpTypes.ts
@@ -0,0 +1,4 @@
+export interface IResultPopUp{
+     show: boolean,
+     handleClose: () => void
+}
