aeed7cf8c5648723668b3a52bb24a685b0445b65
NazarFedus <simpleprograming7@gmail.com>
2023-05-01 08:36:04 +0100

    add logic showing the popup

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,13 @@
 import React, { FC, useContext, useState, useEffect } from "react";
 import { MenuContext } from "../../reducers/context";
 import ProductCard from "../ui/ProductCard/ProductCard";
-import { IProduct, IMenu } from "./MenuTypes";
+import { IProduct } from "./MenuTypes";
+import ResultPopUp from "../ResultPopUp/ResultPopUp";
 
 const Menu: FC = () => {
   const { state } = useContext(MenuContext);
   const [order, setOrder] = useState<string>('');
+  const [showPopUp, setShowPopUp] = useState<boolean>(false)
   console.log(state);
 
   const showOrder = (): string => {
@@ -17,15 +19,20 @@ const Menu: FC = () => {
      return result.filter(e => e !== undefined).join('+')
   }
 
+  const handleToggle = () => {
+     setShowPopUp(!showPopUp)
+  }
+
   useEffect(() => {
      setOrder(showOrder())
   }, [state])
-  
+
   return (
     <div className="flex flex-col w-full">
+     {showPopUp && <ResultPopUp show={showPopUp} handleClose={handleToggle}></ResultPopUp>}
       <div className="flex flex-col flex-[10] bg-white justify-center items-center">
           <h3>{!order ? "You haven't chosen anything yet(" : `Your order: ${order}`}</h3>
-          {order && <button>Get Result</button>}
+          {order && <button className="custom-btn-result" onClick={() => setShowPopUp(true)}>Get Result</button>}
       </div>
       <div className="flex flex-row flex-[90]">
         <div className="category">
--- src/components/ResultPopUp/ResultPopUp.tsx
+++ src/components/ResultPopUp/ResultPopUp.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { FC, useState } from "react";
 
-function ResultPopUp() {
+export default function ResultPopUp({show, handleClose}: {show: boolean, handleClose: () => void}){
+  const [isOpen, setIsOpen] = useState<boolean>(show);
+
+  function togglePopup() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-     <div>
-          
-     </div>
-  )
+    <div className="relative">
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-bold mb-4">Popup Content</h2>
+            <p>Here's some content for the popup!</p>
+            <button
+              onClick={() => {
+                togglePopup(),
+                handleClose()
+              }}
+              className="mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded"
+            >
+              Close Popup
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default ResultPopUp
